b0a93d0e0f83c82873149d3f90f48fb8ef02396a
NoFace546 <sebiosjed@gmail.com>
2024-07-22 17:52:37 +0200

    add functions for computer choice and player choice

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert("Hello world")
+function getComputerChoice(){
+    let number = Math.floor(Math.random() * 3)
+    if (number == 1){
+        console.log("Rock")
+    } else if (number == 2){
+        console.log("Paper")
+    } else{
+        console.log("Scissors")
+    }
+}
+
+function getHumanChoice(){
+    let humanChoice = prompt("Rock, Paper or Scissors?")
+    console.log(humanChoice)
+}
+
+let humanScore = 0, computerScore = 0
+
+
+
+
+getComputerChoice()
+getHumanChoice()
