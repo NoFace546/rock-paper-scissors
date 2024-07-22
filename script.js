a664f5756e7754cc790f463ae2c8e8cfd0f07647
NoFace546 <sebiosjed@gmail.com>
2024-07-22 18:10:49 +0200

    add function to see who wins
    can not score points if we does not have this function

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
 function getComputerChoice(){
     let number = Math.floor(Math.random() * 3)
     if (number == 1){
-        console.log("Rock")
+        return "rock"
     } else if (number == 2){
-        console.log("Paper")
+        return "paper"
     } else{
-        console.log("Scissors")
+        return "scissors"
     }
 }
 
 function getHumanChoice(){
     let humanChoice = prompt("Rock, Paper or Scissors?")
-    console.log(humanChoice)
+    return humanChoice
+}
+
+
+
+function playRound(humanChoice, computerChoice){
+    const humanChoice = humanChoice.lowerCase()
+
+    // Human and AI picks the same
+    if (humanChoice == computerChoice){
+        return "tie"
+    }
+
+    // Human picks Rock
+    if (humanChoice == "rock"){
+        if (computerChoice == "scissors") {
+            return "win"
+        } else {
+            return "lose"
+        }
+    }
+
+    // Human picks Paper
+    if (humanChoice == "paper"){
+        if (computerChoice == "rock") {
+            return "win"
+        } else {
+            return "lose"
+        }
+    }
+
+    // Human picks sicssors
+    if (humanChoice == "scissors"){
+        if (computerChoice == "paper") {
+            return "win"
+        } else {
+            return "lose"
+        }
+    }
 }
 
 let humanScore = 0, computerScore = 0
 
-
-
-
-getComputerChoice()
-getHumanChoice()
